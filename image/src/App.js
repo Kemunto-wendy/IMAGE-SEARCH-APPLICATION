caec2298e6b44e54fae4f81f94c85bdeaa0f3aef
JOY <joy.kemunto@student.moringaschool.com>
2022-07-28 18:57:19 +0300

    changes

--- image/src/App.js
+++ image/src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
+import{useState, useEffect} from "react"
 import './App.css';
-import {LoadImages} from "./components/api"
+import {LoadImages, SearchImages} from "./components/api"
 import Image from "./components/image"
 function App() {
   const [query, setQuery] = useState()
+  const [search, setSearch] = useState()
   const data = LoadImages()
-  console.log(data)
+  console.log(query)
+  const search = () => {
+    setSearch(query)
+  }
+  console.log(SearchImages(query))
   return (
     <div className="App">
     <div>
       <input type= "text" onChange={(event) => setQuery(event.target.value)}/>
-    </div>
-        {data.map((img) => (
-          <Image src={img.urls.thumb} />
+      <button onClick={search}>Search</button>
+      </div>
+        {data.map((img, key) => (
+          <Image src={img.urls.thumb} key={key} />
         ))}
     </div>
   );
--- image/src/components/api.js
+++ image/src/components/api.js
@@ -2,7 +2,7 @@ import{useState, useEffect} from "react"
 import axios from "axios"
 const count = 1
 
-function LoadImages(){
+function LoadImages(query){
     const [state, setState] = useState([])
     useEffect(() => {
         axios.get("https://api.unsplash.com/photos?client_id=6fa91622109e859b1c40218a5dead99f7262cf4f698b1e2cb89dd18fc5824d15")
@@ -13,4 +13,15 @@ function LoadImages(){
     return state
 }
 
-export {LoadImages}
+function SearchImages(query){
+    const [state, setState] = useState([])
+    useEffect(() => {
+        axios.get("https://api.unsplash.com/search/photos?query="+query+"&client_id=6fa91622109e859b1c40218a5dead99f7262cf4f698b1e2cb89dd18fc5824d15")
+        .then((data) =>{
+            console.log(data.data.results)  
+    })
+    }, [query])
+    return state
+}
+
+export {LoadImages, SearchImages}
