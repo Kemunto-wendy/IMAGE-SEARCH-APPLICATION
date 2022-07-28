5c47470449b079ee406f8bddec0f4c4d96f103da
JOY <joy.kemunto@student.moringaschool.com>
2022-07-28 16:45:58 +0300

    changes

--- image/src/components/api.js
+++ image/src/components/api.js
@@ -3,11 +3,13 @@ import axios from "axios"
 
 function LoadImages(){
     const [state, setState] = useState([])
-    axios.get("https://api.unsplash.com/photos?client_id=6fa91622109e859b1c40218a5dead99f7262cf4f698b1e2cb89dd18fc5824d15")
-    .then((data) =>{
-        console.log(data)
+    useEffect(() => {
+        axios.get("https://api.unsplash.com/photos?client_id=6fa91622109e859b1c40218a5dead99f7262cf4f698b1e2cb89dd18fc5824d15")
+        .then((data) =>{
+            setState(data.data)  
     })
-    
+    })
+    return state
 }
 
 export {LoadImages}
