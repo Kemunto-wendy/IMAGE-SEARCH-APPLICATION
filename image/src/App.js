52671e448cc09be9d63c981bb5e86bb72d2f938a
JOY <joy.kemunto@student.moringaschool.com>
2022-07-28 17:34:38 +0300

    changes

--- image/src/App.js
+++ image/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import {LoadImages} from "./components/api"
 import Image from "./components/image"
 function App() {
+  const [query, setQuery] = useState()
   const data = LoadImages()
   console.log(data)
   return (
     <div className="App">
+    <div>
+      <input type= "text" onChange={(event) => setQuery(event.target.value)}/>
+    </div>
         {data.map((img) => (
           <Image src={img.urls.thumb} />
         ))}
